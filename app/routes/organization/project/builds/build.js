0b2ca7e170747ab186bdc05f7a52e9cc2dcc4630
LC <lrc3233@gmail.com>
2018-02-07 11:40:29 -0800

    model is a build, loads snapshots later

--- app/routes/organization/project/builds/build.js
+++ app/routes/organization/project/builds/build.js
@@ -1,27 +1,16 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import {hash} from 'rsvp';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   queryParams: {
     activeSnapshotId: {as: 'snapshot', replace: true},
   },
   model(params) {
-    // TODO can we include snapshots in the build query?
-    const build = this.store.findRecord('build', params.build_id);
-    const snapshots = build.then(build => {
-      return build.get('snapshots');
-    });
-
-    return hash({
-      build,
-      snapshots,
-    });
+    return this.store.findRecord('build', params.build_id);
   },
 
   afterModel(model) {
-    let build = model.build ? model.build : model;
-    build.reload().then(build => {
+    model.reload().then(build => {
       if (!build.get('isExpired')) {
         // Force reload because these async-hasMany's won't reload themselves if the build's
         // state has changed, such as going from processing --> finished and we don't want to show
@@ -40,8 +29,7 @@ export default Route.extend(AuthenticatedRouteMixin, {
     didTransition() {
       this._super.apply(this, arguments);
 
-      let model = this.modelFor(this.routeName);
-      let build = model.build ? model.build : model;
+      let build = this.modelFor(this.routeName);
 
       let organization = build.get('project.organization');
       let eventProperties = {
@@ -56,10 +44,10 @@ export default Route.extend(AuthenticatedRouteMixin, {
       this.set('controller.activeSnapshotId', snapshotId);
     },
     updateModalState(state) {
-      this.get('currentModel').build.set('isShowingModal', state);
+      this.get('currentModel').set('isShowingModal', state);
     },
     openSnapshotFullModal(snapshotId, snapshotSelectedWidth) {
-      let build = this.modelFor(this.routeName).build;
+      let build = this.modelFor(this.routeName);
       let organization = build.get('project.organization');
       let eventProperties = {
         project_id: build.get('project.id'),
